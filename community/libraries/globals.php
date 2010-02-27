f83991ea2e8dad755986933f98755fa52c851ad6
venakis <venakis@b0e902ce-d33f-0410-b758-e8627170a121>
2010-02-27 11:14:06 +0000

    Committing 3.6.0 build 6152 Committing 3.6.0 build 6152 Committing 3.6.0 build 6152 Committing 3.6.0 build 6152 Committing 3.6.0 build 6152
    git-svn-id: https://efrontlearning.svn.sourceforge.net/svnroot/efrontlearning@72 b0e902ce-d33f-0410-b758-e8627170a121

--- community/libraries/globals.php
+++ community/libraries/globals.php
@@ -250,7 +250,7 @@ function setupVersion() {
 function setDefines() {
     /*Get the build number*/
     preg_match("/(\d+)/", '$LastChangedRevision$', $matches);
-    $build = 6147;
+    $build = 6152;
     defined("G_BUILD") OR define("G_BUILD", $build);
     defined("G_BUILD") OR define("G_BUILD", $build);
     /*Define default encoding to be utf-8*/

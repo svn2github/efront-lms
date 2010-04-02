58cb7ce6221974d68fc9b6f32e90e7e0fd1abf82
venakis <venakis@b0e902ce-d33f-0410-b758-e8627170a121>
2010-04-02 14:30:22 +0000

    language files update committing build 6435 committing build 6440 language files update committing build 6435 committing build 6440 language files update committing build 6435 committing build 6440 language files update committing build 6435 committing build 6440 language files update committing build 6435 committing build 6440
    git-svn-id: https://efrontlearning.svn.sourceforge.net/svnroot/efrontlearning@92 b0e902ce-d33f-0410-b758-e8627170a121

--- community/libraries/globals.php
+++ community/libraries/globals.php
@@ -251,7 +251,7 @@ function setupVersion() {
 function setDefines() {
     /*Get the build number*/
     preg_match("/(\d+)/", '$LastChangedRevision$', $matches);
-    $build = 6430;
+    $build = 6440;
     defined("G_BUILD") OR define("G_BUILD", $build);
     defined("G_BUILD") OR define("G_BUILD", $build);
     /*Define default encoding to be utf-8*/

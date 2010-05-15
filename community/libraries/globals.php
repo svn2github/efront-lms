83422ab7b5c5a6598c4e53e099aafee1dafbe28e
venakis <venakis@b0e902ce-d33f-0410-b758-e8627170a121>
2010-05-15 21:48:54 +0000

    Added finished lesson page Added finished lesson page Added finished lesson page Added finished lesson page Added finished lesson page
    git-svn-id: https://efrontlearning.svn.sourceforge.net/svnroot/efrontlearning@136 b0e902ce-d33f-0410-b758-e8627170a121

--- community/libraries/globals.php
+++ community/libraries/globals.php
@@ -260,7 +260,7 @@ function setupVersion() {
 function setDefines() {
     /*Get the build number*/
     preg_match("/(\d+)/", '$LastChangedRevision$', $matches);
-    $build = 6859;
+    $build = 6863;
     defined("G_BUILD") OR define("G_BUILD", $build);
     defined("G_BUILD") OR define("G_BUILD", $build);
     /*Define default encoding to be utf-8*/

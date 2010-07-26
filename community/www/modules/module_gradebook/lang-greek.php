5c228ba57112221d46e97d28601b9de56c796f7e
venakis <venakis@b0e902ce-d33f-0410-b758-e8627170a121>
2010-07-26 16:39:09 +0000

    adding module gradebook
    git-svn-id: https://efrontlearning.svn.sourceforge.net/svnroot/efrontlearning@309 b0e902ce-d33f-0410-b758-e8627170a121

---
+++ community/www/modules/module_gradebook/lang-greek.php
@@ -0,0 +1,40 @@
+<?php
+
+define("_GRADEBOOK_NAME", "Βαθμολόγιο");
+define("_GRADEBOOK_ADD_RANGE", "Προσθήκη διαστήματος");
+define("_GRADEBOOK_EDIT_RANGE", "Επεξεργασία διαστήματος");
+define("_GRADEBOOK_RANGE_FROM", "Από");
+define("_GRADEBOOK_RANGE_TO", "Μέχρι");
+define("_GRADEBOOK_GRADE", "Βαθμός");
+define("_GRADEBOOK_RANGE", "Το διάστημα");
+define("_GRADEBOOK_RANGE_SUCCESSFULLY_ADDED", "Το διάστημα προστέθηκε επιτυχώς");
+define("_GRADEBOOK_RANGE_SUCCESSFULLY_EDITED", "Το διάστημα ανανεώθηκε επιτυχώς");
+define("_GRADEBOOK_RANGE_ADD_PROBLEM", "Το διάστημα δεν προστέθηκε επιτυχώς");
+define("_GRADEBOOK_RANGE_EDIT_PROBLEM", "Το διάστημα δεν ανανεώθηκε επιτυχώς");
+define("_GRADEBOOK_ALREADY_EXISTS", "υπάρχει ήδη");
+define("_GRADEBOOK_GRATER_THAN", "πρέπει να είναι μεγαλύτερο από");
+define("_GRADEBOOK_INVALID_RANGE", "Μη έγκυρο διάστημα");
+define("_GRADEBOOK_STUDENT_NAME", "Μαθητής");
+define("_GRADEBOOK_SCORE", "Σκορ");
+define("_GRADEBOOK_PUBLISH", "Δημοσίευση");
+define("_GRADEBOOK_ADD_COLUMN", "Προσθήκη αντικειμένου (στήλη)");
+define("_GRADEBOOK_COLUMN_NAME", "Όνομα");
+define("_GRADEBOOK_COLUMN_WEIGHT", "Βάρος");
+define("_GRADEBOOK_COLUMN_WEIGHT_DISPLAY", "Βάρος");
+define("_GRADEBOOK_REAL_WORLD_OBJECT", "Αντικείμενο πραγματικού κόσμου (π.χ. τεστ στην τάξη)");
+define("_GRADEBOOK_COLUMN_REFERS_TO", "Αναφέρεται σε");
+define("_GRADEBOOK_COLUMN_SUCCESSFULLY_ADDED", "Το αντικείμενο προστέθηκε επιτυχώς");
+define("_GRADEBOOK_COLUMN_ADD_PROBLEM", "Το αντικείμενο δεν προστέθηκε επιτυχώς");
+define("_GRADEBOOK_DELETE_COLUMN", "Διαγραφή αντικειμένου");
+define("_GRADEBOOK_IMPORT_GRADES", "Εισαγωγή βαθμών για το συγκεκριμένο αντικείμενο");
+define("_GRADEBOOK_INVALID_GRADE", "Μη έγκυρος βαθμός");
+define("_GRADEBOOK_VALID_GRADE_SPECS", "O βαθμός πρέπει να έχει ακέραιη τιμή από 0 έως 100, ή μπορεί να είναι κενός");
+define("_GRADEBOOK_NOACCESS", "Δεν έχετε δικαίωμα να πραγματοποιήσετε την συγκεκριμένη ενέργεια");
+define("_GRADEBOOK_COMPUTE_SCORE_GRADE", "Υπολογισμός Βαθμού/Σκορ");
+define("_GRADEBOOK_EXPORT_EXCEL", "Εξαγωγή σε Excel");
+define("_GRADEBOOK_EXPORT_EXCEL_ONE", "Τρέχον μάθημα");
+define("_GRADEBOOK_ALL_LESSONS", "Όλα τα μαθήματα μου");
+define("_GRADEBOOK_SWITCH_TO", "Μετάβαση σε");
+define("_GRADEBOOK_NOT_PUBLISHED", "Το βαθμολόγιό σας δεν έχει δημοσιευθεί ακόμη");
+
+?>

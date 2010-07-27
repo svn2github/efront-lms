c18f46132af4cc32c1f47a814462ebc15e3b34ab
venakis <venakis@b0e902ce-d33f-0410-b758-e8627170a121>
2010-07-27 07:14:25 +0000

    committing 3.6.5 8007 committing 3.6.5 8007 committing 3.6.5 8007 committing 3.6.5 8007 committing 3.6.5 8007
    git-svn-id: https://efrontlearning.svn.sourceforge.net/svnroot/efrontlearning@311 b0e902ce-d33f-0410-b758-e8627170a121

--- community/libraries/globals.php
+++ community/libraries/globals.php
@@ -261,7 +261,7 @@ function setupVersion() {
 function setDefines() {
     /*Get the build number*/
     preg_match("/(\d+)/", '$LastChangedRevision$', $matches);
-    $build = 8007;
+    $build = 8013;
     defined("G_BUILD") OR define("G_BUILD", $build);
     /*Define default encoding to be utf-8*/
     mb_internal_encoding('utf-8');

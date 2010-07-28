a03e82db97f5a2917252d975c4c4e913a4a7bc4e
venakis <venakis@b0e902ce-d33f-0410-b758-e8627170a121>
2010-07-28 17:14:33 +0000

    Committing new 3.6.5 version (8086) Committing new 3.6.5 version (8086) Committing new 3.6.5 version (8086) Committing new 3.6.5 version (8086) Committing new 3.6.5 version (8086)
    git-svn-id: https://efrontlearning.svn.sourceforge.net/svnroot/efrontlearning@323 b0e902ce-d33f-0410-b758-e8627170a121

--- community/libraries/globals.php
+++ community/libraries/globals.php
@@ -260,7 +260,7 @@ function setupVersion() {
 function setDefines() {
     /*Get the build number*/
     preg_match("/(\d+)/", '$LastChangedRevision$', $matches);
-    $build = 8086;
+    $build = 8087;
     defined("G_BUILD") OR define("G_BUILD", $build);
     /*Define default encoding to be utf-8*/
     mb_internal_encoding('utf-8');

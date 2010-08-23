82de13653e72f06a9ed83f585a480dc1b79c7776
venakis <venakis@b0e902ce-d33f-0410-b758-e8627170a121>
2010-08-23 18:14:44 +0000

    Committing 3.6.6 build 8231 Committing 3.6.6 build 8236
    git-svn-id: https://efrontlearning.svn.sourceforge.net/svnroot/efrontlearning@341 b0e902ce-d33f-0410-b758-e8627170a121

--- community/libraries/globals.php
+++ community/libraries/globals.php
@@ -79,7 +79,6 @@ isset($configuration['max_execution_time']) && $configuration['max_execution_tim
 isset($GLOBALS['configuration']['time_zone']) && isset($GLOBALS['configuration']['time_zone']) ? date_default_timezone_set($GLOBALS['configuration']['time_zone']) : null;
 ini_set('magic_quotes_runtime', false); // check http://www.smarty.net/forums/viewtopic.php?t=4936
 //handleSEO();
-
 //Setup the current version
 setupVersion();
 
@@ -263,7 +262,7 @@ function setupVersion() {
 function setDefines() {
     /*Get the build number*/
     preg_match("/(\d+)/", '$LastChangedRevision$', $matches);
-    $build = 8231;
+    $build = 8236;
     defined("G_BUILD") OR define("G_BUILD", $build);
     /*Define default encoding to be utf-8*/
     mb_internal_encoding('utf-8');
--- community/libraries/includes/statistics/advanced_user_reports.php
+++ community/libraries/includes/statistics/advanced_user_reports.php
@@ -1,1 +1,7 @@
 <?php
+
+  //This file cannot be called directly, only included.
+  if (str_replace(DIRECTORY_SEPARATOR, "/", __FILE__) == $_SERVER['SCRIPT_FILENAME']) {
+   exit;
+  }
+?>

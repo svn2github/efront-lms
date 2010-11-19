acfa14a92808b164aab35dd26229c21799fe8ddd
venakis <venakis@b0e902ce-d33f-0410-b758-e8627170a121>
2010-11-19 21:51:25 +0000

    minor updates committing efront 3.6.7 build 9006
    git-svn-id: https://efrontlearning.svn.sourceforge.net/svnroot/efrontlearning@414 b0e902ce-d33f-0410-b758-e8627170a121

--- community/libraries/globals.php
+++ community/libraries/globals.php
@@ -247,7 +247,7 @@ function setupVersion() {
 function setDefines() {
     /*Get the build number*/
     preg_match("/(\d+)/", '$LastChangedRevision$', $matches);
-    $build = 9001;
+    $build = 9006;
     defined("G_BUILD") OR define("G_BUILD", $build);
     /*Define default encoding to be utf-8*/
     mb_internal_encoding('utf-8');

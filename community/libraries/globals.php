fd17707ec3df5b8df243b75f2df6f7190e8a420f
venakis <venakis@b0e902ce-d33f-0410-b758-e8627170a121>
2010-12-05 20:48:34 +0000

    committing 3.6.7 9135
    git-svn-id: https://efrontlearning.svn.sourceforge.net/svnroot/efrontlearning@429 b0e902ce-d33f-0410-b758-e8627170a121

--- community/libraries/globals.php
+++ community/libraries/globals.php
@@ -13,6 +13,7 @@ if (str_replace(DIRECTORY_SEPARATOR, "/", __FILE__) == $_SERVER['SCRIPT_FILENAME
     exit;
 }
 
+
 //Used for debugging purposes only
 $debug_TimeStart = microtime(true);
 
@@ -247,7 +248,7 @@ function setupVersion() {
 function setDefines() {
     /*Get the build number*/
     preg_match("/(\d+)/", '$LastChangedRevision$', $matches);
-    $build = 9134;
+    $build = 9136;
     defined("G_BUILD") OR define("G_BUILD", $build);
     /*Define default encoding to be utf-8*/
     mb_internal_encoding('utf-8');

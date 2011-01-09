27e4d3dd0eb6dfb1ca467fc4b394a5747dcd0708
venakis <venakis@b0e902ce-d33f-0410-b758-e8627170a121>
2011-01-09 08:04:51 +0000

    removed flash graph files
    git-svn-id: https://efrontlearning.svn.sourceforge.net/svnroot/efrontlearning@447 b0e902ce-d33f-0410-b758-e8627170a121

--- community/libraries/globals.php
+++ community/libraries/globals.php
@@ -15,7 +15,6 @@ if (str_replace(DIRECTORY_SEPARATOR, "/", __FILE__) == $_SERVER['SCRIPT_FILENAME
 //Used for debugging purposes only
 $debug_TimeStart = microtime(true);
 
-
 /**
  * Set debugging level:
  * 0: no error reporting
@@ -250,7 +249,7 @@ function setupVersion() {
 function setDefines() {
     /*Get the build number*/
     preg_match("/(\d+)/", '$LastChangedRevision$', $matches);
-    $build = 9441;
+    $build = 9446;
     defined("G_BUILD") OR define("G_BUILD", $build);
     /*Define default encoding to be utf-8*/
     mb_internal_encoding('utf-8');

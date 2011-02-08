16ca7c0ae2e13777abdaeb8a9e398e7c8841875e
venakis <venakis@b0e902ce-d33f-0410-b758-e8627170a121>
2011-02-08 14:48:59 +0000

    committing 3.6.8 build 9931
    git-svn-id: https://efrontlearning.svn.sourceforge.net/svnroot/efrontlearning@491 b0e902ce-d33f-0410-b758-e8627170a121

--- community/libraries/globals.php
+++ community/libraries/globals.php
@@ -13,6 +13,7 @@ if (str_replace(DIRECTORY_SEPARATOR, "/", __FILE__) == $_SERVER['SCRIPT_FILENAME
 }
 
 
+
 //Used for debugging purposes only
 $debug_TimeStart = microtime(true);
 
@@ -251,7 +252,7 @@ function setupVersion() {
 function setDefines() {
     /*Get the build number*/
     preg_match("/(\d+)/", '$LastChangedRevision$', $matches);
-    $build = 9924;
+    $build = 9931;
     defined("G_BUILD") OR define("G_BUILD", $build);
     /*Define default encoding to be utf-8*/
     mb_internal_encoding('utf-8');

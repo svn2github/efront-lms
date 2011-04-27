07c7915b8a580a5a31623aed05a3bb8ac7ab8450
venakis <venakis@b0e902ce-d33f-0410-b758-e8627170a121>
2011-04-27 10:33:58 +0000

    Fixed mapped accounts disregarding system setting fixed personal profile fields appearing even if meaningless Fixed clicking on icons including a link with popups, does not load the popup Fixed subforum/topics count in forums list Corrections by Ermani Added icon about deleting not installed modules job courses in administrator tools only in enterprise edition Fixed issue about displaying sender e-mail address in subject in contact form German language file by shalb
    fixed resume test
    fixed bypassing agreement through switch account
    user_profile db table fix 
    fixed language options appearing when creating a new user, even if single language is set
    fix small js issue 
    fix issue about file manager in questions tab
    - Added option to hide parts of organizational chart that are not related to a user (Enterprise edition)
    committing Version 3.6.9 build 10697
    
    git-svn-id: https://efrontlearning.svn.sourceforge.net/svnroot/efrontlearning@537 b0e902ce-d33f-0410-b758-e8627170a121

--- community/libraries/globals.php
+++ community/libraries/globals.php
@@ -13,7 +13,6 @@ if (str_replace(DIRECTORY_SEPARATOR, "/", __FILE__) == $_SERVER['SCRIPT_FILENAME
 }
 
 
-
 //Used for debugging purposes only
 $debug_TimeStart = microtime(true);
 
@@ -268,7 +267,7 @@ function setupVersion() {
 function setDefines() {
     /*Get the build number*/
     preg_match("/(\d+)/", '$LastChangedRevision$', $matches);
-    $build = 10660;
+    $build = 10697;
     defined("G_BUILD") OR define("G_BUILD", $build);
     /*Define default encoding to be utf-8*/
     mb_internal_encoding('utf-8');
